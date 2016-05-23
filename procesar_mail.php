4e7a2cca6de1f25025eee1ac25ff85a2ea628cf0
sosaheri <sosaheriberto2001@gmail.com>
2016-05-23 13:22:59 -0400

    x
    asd

---
+++ procesar_mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$destinatario = "info@exodatasolutions.com";
+$subject = "Consulta De Servicios En Exodata Solutions";
+$mensaje = "Srs. Administradores \n\n{$_POST['nombre']} le ha enviado un mensaje.\n\n
+Dice lo siguiente:\n\n {$_POST['mensaje']}\n\n
+correo de contacto: {$_POST['correo']}\n\n ";
+$cabecera = 'From: Pagina Web <info@exodatasolutions.com>'. "\r\n";
+
+mail($destinatario, $subject, $mensaje, $cabecera);
+
+?>
+
