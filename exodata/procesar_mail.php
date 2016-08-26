d71d02ff573d2f9c1bfff9ec3b48c5d0ea2c0633
sosaheri <sosaheriberto2001@gmail.com>
2016-08-26 10:51:46 -0400

    php
    php

---
+++ exodata/procesar_mail.php
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+<?php
+
+$destinatario = "robert.ochoa@gmail.com";
+$subject = "Consulta De Servicios En Exodata Solutions";
+$mensaje = "Srs. Administradores \n\n{$_POST['nombre']} le ha enviado un mensaje.\n\n
+Dice lo siguiente:\n\n {$_POST['mensaje']}\n\n
+correo de contacto: {$_POST['correo']}\n\n ";
+$cabecera = 'From: Pagina Web <info@exodatasolutions.com>'. "\r\n";
+
+mail($destinatario, $subject, $mensaje, $cabecera);
+
+?>
+
+=======
+<?php
+
+$destinatario = "sosaheriberto2001@gmail.com";
+$subject = "Consulta De Servicios En Exodata Solutions";
+$mensaje = "Srs. Administradores \n\n{$_POST['nombre']} le ha enviado un mensaje.\n\n
+Dice lo siguiente:\n\n {$_POST['mensaje']}\n\n
+correo de contacto: {$_POST['correo']}\n\n ";
+$cabecera = 'From: Pagina Web <info@exodatasolutions.com>'. "\r\n";
+
+mail($destinatario, $subject, $mensaje, $cabecera);
+
+?>
+
+>>>>>>> origin/gh-pages
